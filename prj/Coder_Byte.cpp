bfea0e5f391238559c4fd5628950f3b4874db9a6
sudhakar jha <jhasudhakar.ai@gmail.com>
2020-09-03 22:55:43 +0530

    adding the visual studio solution

---
+++ prj/Coder_Byte.cpp
@@ -0,0 +1,10 @@
+// Coder_Byte.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Program name= " << argv[0] << std::endl;
+}
